257b2361eef5f922b47b7857c2bd40c54ae1fcd6
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-08-05 13:33:11 +0300

    hiking animation

---
+++ Hiking/Hiking/GraphCapsule.swift
@@ -0,0 +1,38 @@
+//
+//  GraphCapsule.swift
+//  Hiking
+//
+//  Created by Harun Sasmaz on 4.08.2020.
+//  Copyright © 2020 Harun Sasmaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct GraphCapsule: View {
+    var index: Int
+    var height: CGFloat
+    var range: Range<Double>
+    var overallRange: Range<Double>
+    
+    var heightRatio: CGFloat {
+        max(CGFloat(magnitude(of: range) / magnitude(of: overallRange)), 0.15)
+    }
+    
+    var offsetRatio: CGFloat {
+        CGFloat((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
+    }
+    
+    var body: some View {
+        Capsule()
+            .fill(Color.white)
+            .frame(height: height * heightRatio)
+            .offset(x: 0, y: height * -offsetRatio)
+    }
+}
+
+struct GraphCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphCapsule(index: 0, height: 150, range: 10..<50, overallRange: 0..<100)
+    }
+}
+
---
+++ Hiking/Hiking/Hike.swift
@@ -0,0 +1,32 @@
+//
+//  Hike.swift
+//  Hiking
+//
+//  Created by Harun Sasmaz on 4.08.2020.
+//  Copyright © 2020 Harun Sasmaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct Hike: Codable, Hashable, Identifiable {
+    var name: String
+    var id: Int
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        return Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
